a735892fbeb35c6a9264812ce12892abdc20024d
Daniel Broyde <danielbroyde.work@gmail.com>
2020-10-13 13:39:58 +0300

    first commit

---
+++ MParticalTest/UI/First/FirstYellowViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FirstYellowViewController.swift
+//  MParticalTest
+//
+//  Created by Daniel on 08/10/2020.
+//  Copyright © 2020 Daniel. All rights reserved.
+//
+
+import UIKit
+
+class FirstYellowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MParticalTest/UI/Second/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  MParticalTest
+//
+//  Created by Daniel on 08/10/2020.
+//  Copyright © 2020 Daniel. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
